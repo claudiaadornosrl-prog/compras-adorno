0a171fe73a723262c11da84e0f98cb8934622fd9
Claudia Adorno <claudiaadornosrl@gmail.com>
2026-09-11 14:25:07 -0300

    deploy 2026-09-11 14:25

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'compras-v23-z-sin-gestion';
+const CACHE_VERSION = 'compras-v24-desc-concepto';
 const ASSETS = ['./', './index.html', './manual.js', './manifest.webmanifest',
                 './fonts/URWGothic-Book.ttf'];
 self.addEventListener('install', e => {
